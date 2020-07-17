10267dde21eaccbdc6a003485f20354c08f08736
ChrisGilliam <crsgilliam@gmail.com>
2020-07-17 17:43:54 +1000

    Update to Orient_Est_DS_V2

--- Orient_Est_DS_V2.m
+++ Orient_Est_DS_V2.m
@@ -1,18 +1,10 @@
-function Angle = Orient_Est_DS_V2(im,W,num_scales,trig_average)
-
-if nargin == 2
-    num_scales = 1;
-    trig_average = 0;
-elseif nargin == 3
-    trig_average = 0;
-end
-
-num_scales = 1;
-trig_average = 0;
+function Angle = Orient_Est_DS_V2(im,W)
+% function determines the dominate 2D orientation of texture in the volume 
+% im. The computation is performed in a local window of size W by W by W.
 
 %%
 % Define size and sigma for Gaussian filters
-s = 3;      % Size of 3D Gaussian filters (filters are s by s by 2 voxels in size)
+s = 6;      % Size of 3D Gaussian filters (filters are s by s by 2 voxels in size)
 sigma = (s+2)/4;  % Sigma value of the 3D Gaussians (assuming symmetrical Gaussians)
 
 % determine size of image:
@@ -75,11 +67,11 @@ theta = acos(Gz./G);
 %--------------------------------------------------------------------------
 
 % determine a vector of possible orientations:
-res = 2;
-angle_vector1 = 0:res:(180-res);
-N_angle1 = numel(angle_vector1);
-angle_vector2 = 0:res:(180-res);
+res = 1;
+angle_vector2 = [0,res/2,res:res:(180-res),(180-res/2)];
 N_angle2 = numel(angle_vector2);
+angle_vector1 = [0,res/2,res:res:(180-res),(180-res/2)];
+N_angle1 = numel(angle_vector1);
 
 
 %% 2D Search
@@ -105,60 +97,22 @@ end
 % find the minimum solution:
 [~,i2] = min(reshape(A,M,N,P,N_angle1*N_angle2),[],4);
 [i1,i2] = ind2sub([N_angle1,N_angle2],i2(:));
-Dom_angle = reshape(angle_vector2(i2),M,N,P);           % phi
-Dom_angle(:,:,:,2) = reshape(angle_vector1(i1),M,N,P);  % theta
+Dom_angle_phi = reshape(angle_vector2(i2),M,N,P);           % phi
+Dom_angle_theta = reshape(angle_vector1(i1),M,N,P);         % theta
 
 % Determine sub pixel accuracy:
 [Delta_theta,Delta_phi]=subSample2D(A,i1,i2,res,res);
-Dom_angle(:,:,:,1) = Dom_angle(:,:,:,1) + reshape(Delta_phi,M,N,P);
-Dom_angle(:,:,:,2) = Dom_angle(:,:,:,2) + reshape(Delta_theta,M,N,P);
+Dom_angle_phi = Dom_angle_phi + reshape(Delta_phi,M,N,P);
+Dom_angle_theta = Dom_angle_theta + reshape(Delta_theta,M,N,P);
 
-%% 2 1D Searches
-% % Initialise 4D Matrix to hold data
-% A = zeros(M,N,P,N_angle1);
-% 
-% theta_est = pi/2;
-% 
-% % Determine phi based on theta_est=90 degrees:
-% for index1 = 1:N_angle1
-%     % obtain local value of theta and conver to radians:
-%     phi_est = angle_vector1(index1)*pi/180;
-%     % calculate test:
-%     holder = G.*abs(cos(theta_est).*cos(theta) + sin(theta_est).*sin(theta).*cos((phi_est-phi)));
-%     % perform local summation:
-%     A(:,:,:,index1,1) = local_sum(holder,W);
-% end
-% [~,i2] = min(A,[],4);
-% Dom_angle = angle_vector1(i2);
-% 
-% % Interp peak:
-% Delta=subSample(A,i2,res);
-% Dom_angle = Dom_angle+reshape(Delta,M,N,P);
-% 
-% A = zeros(M,N,P,N_angle2);
-% % phi_est = Dom_angle.*pi./180;
-% phi_est = pi/5;
-% for index1 = 1:N_angle2
-%     % obtain local value of theta and conver to radians:
-%     theta_est = angle_vector2(index1)*pi/180;
-%     % calculate test:
-%     holder = G.*abs(cos(theta_est).*cos(theta) + sin(theta_est).*sin(theta).*cos((phi_est-phi)));
-%     % perform local summation:
-%     A(:,:,:,index1) = local_sum(holder,W);
-% end
-% [~,i2] = min(A,[],4);
-% Dom_angle(:,:,:,2) = angle_vector2(i2);
-% 
-% % Interp peak:
-% Delta=subSample(A,i2,res);
-% Dom_angle(:,:,:,2) = Dom_angle(:,:,:,2)+reshape(Delta,M,N,P);
+% Deal with any cases where phi<0:
+index = Dom_angle_phi<0;
+Dom_angle_phi(index) = Dom_angle_phi(index)+180;
+Dom_angle_theta(index) = 180-Dom_angle_theta(index);
 
 % Convert to radian:
-Angle = Dom_angle.*pi/180;
-
-if trig_average == 1
-    Angle = angle(local_sum(exp(1i.*2.*Angle),W))./2;
-end
+Angle = Dom_angle_phi.*pi/180;
+Angle(:,:,:,2) = Dom_angle_theta.*pi/180;
 
 
 end
@@ -175,31 +129,6 @@ J = imfilter(shiftdim(J,1), FA,'symmetric');
 J = shiftdim(J,1)/(W*W*W);
 end
 
-function Delta=subSample(A,index2,res)
-%% Determine subsample accuracy of a peak
-% Size
-[M,N,P,Q] = size(A);
-
-% indexing coordinates:
-[Y,X,Z] = meshgrid(1:M,1:N,1:P);
-
-% Interp peak:
-index1 = index2-1;
-index1(index1==0) = Q;
-R1 = A(sub2ind([M,N,P,Q],X(:),Y(:),Z(:),index1(:)));
-R2 = A(sub2ind([M,N,P,Q],X(:),Y(:),Z(:),index2(:)));
-index3 = index2+1;
-index3(index3==Q+1) = 1;
-R3 = A(sub2ind([M,N,P,Q],X(:),Y(:),Z(:),index3(:)));
-
-% Gaussian Interp:
-Delta=((log(R3(:))-log(R1(:)))./(4*log(R2(:))-2*log(R1(:))-2*log(R3(:)))).*res;
-% % parabola Interp:
-% Delta=((R3(:)-R1(:))./(2*(2*R2(:)-R1(:)-R3(:)))).*res;
-
-Delta(isnan(Delta)) = 0;
-end
-
 function [Delta_x,Delta_y]=subSample2D(Data,index_x,index_y,res_x,res_y)
 %% Determine subsample accuracy of a peak based on 2D fitting using the
 % form y = Ax^2 + By^2 + Cx + Dy + Exy + F
@@ -211,39 +140,60 @@ function [Delta_x,Delta_y]=subSample2D(Data,index_x,index_y,res_x,res_y)
 [Y,X,Z] = meshgrid(1:M,1:N,1:P);
 
 % Define x indices:
-index_x1 = index_x-1;
-index_x1(index_x1==0) = Q;
-index_x3 = index_x+1;
-index_x3(index_x3==Q+1) = 1;
+index_x11 = index_x-1;
+index_x11(index_x11==0) = Q;
+index_x12 = index_x11;
+index_x13 = index_x11;
+index_x21 = index_x;
+index_x23 = index_x;
+index_x31 = index_x+1;
+index_x31(index_x31==Q+1) = 1;
+index_x32 = index_x31;
+index_x33 = index_x31;
 
 % Define y indices:
 index_y1 = index_y-1;
-index_y1(index_y1==0) = R;
+holder1 = logical(index_y1==0);
+index_y1(holder1==1) = R;
+index_x11(holder1==1) = Q-index_x11(holder1==1)+2;
+index_x21(holder1==1) = Q-index_x21(holder1==1)+2;
+index_x31(holder1==1) = Q-index_x31(holder1==1)+2;
+index_x11(index_x11==Q+1) = 1;
+index_x21(index_x21==Q+1) = 1;
+index_x31(index_x31==Q+1) = 1;
+
 index_y3 = index_y+1;
-index_y3(index_y3==R+1) = 1;
+holder1 = logical(index_y3==(R+1));
+index_y3(holder1==1) = 1;
+index_x13(holder1==1) = Q-index_x13(holder1==1)+2;
+index_x23(holder1==1) = Q-index_x23(holder1==1)+2;
+index_x33(holder1==1) = Q-index_x33(holder1==1)+2;
+index_x13(index_x13==Q+1) = 1;
+index_x23(index_x23==Q+1) = 1;
+index_x33(index_x33==Q+1) = 1;
 
 % Obtain coefficients:
-F = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x(:),index_y));
+F = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x(:),index_y(:)));
 
 % determine A:
-A = 0.5.*(Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x3(:),index_y)) + ...
-    Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x1(:),index_y))) - F;
+A = 0.5.*(Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x32(:),index_y(:))) + ...
+    Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x12(:),index_y(:)))) - F;
 
 % determine B:
-B = 0.5.*(Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x(:),index_y3)) + ...
-    Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x(:),index_y1))) - F;
+B = 0.5.*(Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x23(:),index_y3(:))) + ...
+    Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x21(:),index_y1(:)))) - F;
 
 % determine C:
-Z1 = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x3(:),index_y3));
-C = 0.5.*(Z1 + Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x3(:),index_y1)))...
+Z1 = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x33(:),index_y3(:)));
+C = 0.5.*(Z1 + Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x31(:),index_y1(:))))...
     -A-B-F;
 
 % determine D:
-D = 0.5.*(Z1 + Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x1(:),index_y3)))...
+D = 0.5.*(Z1 + Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x13(:),index_y3(:))))...
     -A-B-F;
 
 % Determine E:
-E = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x1(:),index_y1)) - A- B + C + D - F;
+E = Data(sub2ind([M,N,P,Q,R],X(:),Y(:),Z(:),index_x11(:),index_y1(:))) - A- B + C + D - F;
 
 % determine shifts:
 Denom = 4.*A.*B - E.^2;
@@ -251,10 +201,12 @@ Denom = 4.*A.*B - E.^2;
 % x:
 Delta_x = -(2.*B.*C-D.*E)./Denom.*res_x;
 Delta_x(isnan(Delta_x)) = 0;
+Delta_x(abs(Delta_x)>res_x)=0;
 
 % y:
 Delta_y = -(2.*A.*D-C.*E)./Denom.*res_y;
 Delta_y(isnan(Delta_y)) = 0;
+Delta_y(abs(Delta_y)>res_y)=0;
 end
 
 function G_out = Quick3DFilter(im,Gd,G,index)
